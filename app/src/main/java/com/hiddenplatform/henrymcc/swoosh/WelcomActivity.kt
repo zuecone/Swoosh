065bbf000153bec3e890277cf4b69394e274d912
Henry McC <henrymcc@gmail.com>
2017-10-01 22:16:38 +0200

    Initial Commit

---
+++ app/src/main/java/com/hiddenplatform/henrymcc/swoosh/WelcomActivity.kt
@@ -0,0 +1,12 @@
+package com.hiddenplatform.henrymcc.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcom)
+    }
+}
